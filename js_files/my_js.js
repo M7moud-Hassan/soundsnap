f846771ffed4331ab5be8ffa5de28974e5348303
mahmoud hassan <abosamour990@gmail.com>
2022-12-09 18:13:14 +0200

    responsible header

---
+++ js_files/my_js.js
@@ -0,0 +1,15 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
+function ToggleSearchBar(){
+    if(document.getElementById("searchBar").style.display=="none"){
+        document.getElementById("searchBar").style.display="inline-block";
+    }else{
+        document.getElementById("searchBar").style.display="none";
+    }
+   
+}  
