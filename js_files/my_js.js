a259610adb4427f13a68db01142751d2e3883853
mahmoud hassan <abosamour990@gmail.com>
2022-12-09 21:04:06 +0200

    main

--- js_files/my_js.js
+++ js_files/my_js.js
@@ -8,7 +8,9 @@ function closeNav() {
 function ToggleSearchBar(){
     if(document.getElementById("searchBar").style.display!="none"){
         document.getElementById("searchBar").style.display="none";
+        document.getElementById("main").style.top="4.5em";
     }else{
         document.getElementById("searchBar").style.display="inline-block";
+        document.getElementById("main").style.top="8em";
     }
 }  
