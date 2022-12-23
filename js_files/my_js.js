ad192fc090ee8cdff82404f15f4ce8ec96ac0f1c
mahmoud hassan <abosamour990@gmail.com>
2022-12-23 16:02:02 +0200

    repair header

--- js_files/my_js.js
+++ js_files/my_js.js
@@ -1,10 +1,12 @@
+//open and close nav
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
   }
-  
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
+
+//toggle search bar  
 function ToggleSearchBar(){
     if(document.getElementById("searchBar").style.display!="none"){
         document.getElementById("searchBar").style.display="none";
@@ -14,6 +16,7 @@ function ToggleSearchBar(){
 }  
 
 
+//show login and sing up
 var logIn = document.getElementById('logIn');
 var signUp = document.getElementById('signUp');
 
@@ -34,4 +37,38 @@ function openSignUp(event){
   event.preventDefault();
   signUp.style.display='block';
  
+}
+
+//clear text search
+var inputSearch =document.getElementById('search');
+var claerSpan=document.getElementById('clear');
+inputSearch.addEventListener('keypress',function(){
+  claerSpan.innerHTML='';
+  claerSpan.classList.add('load');
+  setTimeout(function(){
+    claerSpan.classList.remove('load')
+    claerSpan.innerHTML='&times;';
+  },1000);
+})
+
+function checkClear(){
+  if(inputSearch.value==''){
+    claerSpan.innerHTML='';
+   }
+}
+function clearInput(){
+  inputSearch.value='';
+  claerSpan.innerHTML='';
+}
+
+
+//change type search
+function ChangeList(ele){
+  document.getElementById('typeSearch').innerHTML=ele.innerHTML;
+  inputSearch.placeholder='search for '+ele.innerHTML;
+  if(ele.innerHTML=='Sound Effects'){
+    document.getElementById('dropMenu').style.left='-10%';
+  }else{
+    document.getElementById('dropMenu').style.left='-80%';
+  }
 }
